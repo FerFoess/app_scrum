9370186cedba01eab23c2d1a20a5c847081f3580
Fer Martínez <fr663385@gmail.com>
2024-03-03 16:51:01 -0600

    segundo commit xdd

--- lib/screen.dart
+++ lib/screen.dart
@@ -28,34 +28,39 @@ class _JsonListViewScreenState extends State<JsonListViewScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Scrum Elements'),
-        backgroundColor: Color.fromARGB(255, 32, 69, 255),
-      ),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ItemDetailScreen(item: items[index])),
-                );
-              },
-              child: Text(items[index].name),
-              style: ElevatedButton.styleFrom(
-                primary: const Color.fromARGB(255, 178, 178, 178), 
-                textStyle: const TextStyle(fontSize: 18),
-                padding: const EdgeInsets.all(20), 
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('Scrum Elements'),
+      backgroundColor: Color.fromARGB(255, 32, 69, 255),
+    ),
+    body: Center(
+      child: Container(
+        width: 300, // Definir el ancho deseado del contenedor
+        child: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ItemDetailScreen(item: items[index])),
+                  );
+                },
+                child: Text(items[index].name),
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 0, 0, 0), 
+                  textStyle: const TextStyle(fontSize: 18),
+                  padding: const EdgeInsets.all(20), 
+                ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
-    );
-  }
+    ),
+  );
+}
 }
